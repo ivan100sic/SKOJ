6863667de454c0c47698dd7e4ccef331075afbde
LatentVirtue <asdf@asdf.org>
2017-05-02 22:18:35 +0200

    Sitan ali bitan
    Rad na funkcionalnosti, i test. Pogledaj klasu Div
    Osnovna funkcija sluzi, ali mora da se doradi, sto cu kasnije; Sutra
    planiram sesije da implementiram, i nivoe korisnika.

--- www/index.php
+++ www/index.php
@@ -1,5 +1,11 @@
 <?php
-	require_once('syntax-parse.php');
-	echo "Hi!";
-	echo "<a href='index2.php'>dear fucking god</a>"
+	require_once 'dom.php';
+	class Index extends Page{
+		function __construct(){
+			parent::__construct();
+			$this->toolbar->remove('index');
+		}
+	}
+	$page=new Index();
+	$page->render();
 ?>
