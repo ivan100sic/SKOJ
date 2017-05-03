ab5d679a4373a5b6fdc56332a8c9f99e0c3540f3
LatentVirtue <asdf@asdf.org>
2017-05-03 03:28:43 +0200

    Login forma, CSS i quality of life
    vidi page source pre, i posle commit-a
    probaj da skrolujes na pocetnoj strani
    jebeni css :100:  :anger:  :angry:

--- www/dom.php
+++ www/dom.php
@@ -8,7 +8,7 @@
 		}
 		
 		function render() {
-			echo $this->data;
+			echo $this->data, PHP_EOL;
 		}
 
 		/* Getters, setters... */
@@ -27,27 +27,21 @@
 		}
 		
 		function render() {
-			echo EscapedText::convert($this->data);
+			echo EscapedText::convert($this->data),PHP_EOL;
 		}
 
 		/* Getters, setters... */
 	}
 	class Div{ //class used to create and maintain boxed elements on page. 
 		//Example: individual news stories should each be a Div object
-		private $id; //added this so different box styles can be implemented
-		private $elements;// i meant for div elements to contain Text or EscapedText objects
-		function __construct($id,$element){ //dumb to pass whole array, better to give each elems individually
-			$this->id=$id;
+		protected $class; //m'lady
+		protected $s;
+		protected $elements;
+		function __construct($class,$element){ //agreed on passing array as argument
+			$this->class=$class;
 			$this->elements=$element;
-			//overloads on hold for testing
-			//rethink this, reason: function with array as arg better than function with infinite args
-			/*$this->elements=null;
-			foreach (func_get_args() as $arg){ //default constructor. more to be added as the need arises
-				$this->elements[]= $arg;
-			}*/
 		}
 		function add($key,$element){
-			//todo: implement overloads as in function below, to add the ability to create a default box with only one argument, in which name=null
 			$this->elements[$key]=$element;
 		}
 		function remove(){ //if no args, removes the last element, else it deletes the elements with keys given as arguments
@@ -59,13 +53,42 @@
 				}
 			}
 		}
+		function style(){ //for additional control, meant for position and alignment
+			$ret="style='";
+			foreach(func_get_args() as $arg){
+				$ret=$ret." ".$arg;
+			}
+			$ret=$ret."'";
+			$this->s=$ret;
+		}
 		function render(){ //todo: expand and adapt to determine individual div position, alignment, etc.
-			echo"<div id=$this->id>";
+			echo"<div class='".$this->class."' ".$this->s.">",PHP_EOL;
 			foreach ($this->elements as $key => $value){
 				$value->render();
 				//echo"<br>"; //idk if this should be here, or somewhere else
 			}
-			echo"</div>";
+			echo"</div>",PHP_EOL;
+		}
+	}
+	
+	//Login form
+	//Think about implementing tables as class to help sorting and ordering html elements
+	class Login extends Div{
+		function __construct(){
+			$this->class="login";
+			$this->elements=[
+					'title'=>new Text("<strong>Login:</strong><br>"),
+					'name'=>new Text("Username "),
+					'inputname'=>new Text("<input type='text' name='username'><br>"),
+					'pass'=>new Text("Password "),
+					'inputpass'=>new Text("<input type='password' name='password'><br>"),
+					'submit'=> new Text("<input type='submit'><br>")
+			];
+		}
+		function render(){
+			echo "<form action='action.php' method='post'>",PHP_EOL;
+			parent::render();
+			echo "</form>",PHP_EOL;
 		}
 	}
 
@@ -73,7 +96,6 @@
 		protected $head_items;
 		protected $body_items;
 		protected $toolbar;
-		//protected $style; //for css, is not needed, i can just put STYLE item in HEAD_ITEMS
 		
 		function __construct() {
 			$this->head_items = [
@@ -82,25 +104,17 @@
 				"style" => new Text(" <link rel='stylesheet' href='style.css'>")
 			];
 			$this->body_items = [
-				new Text("Hello world!")
+				//new Text("Hello world!")
 			];
-			//toolbar trough the new class
 			$temp=[
-					'index' => new Text("<a href='index.php'>Home</a><br/>"), //all pages have back to index, except the index page, so it seems more efficient to just put the link in the default Page class
+					'index' => new Text("<a href='index.php'>Home</a><br/>"),
 					'user' => new Text("<a href='user.php'>User</a><br/>"),
 					'task' => new Text("<a href='task.php'>Task</a><br/>"),
 					//add more
 			];
-			//maybe implement toolbar as child of Div
+			//Toolbar style
 			$this->toolbar= new Div("toolbar",$temp);
 			$this->toolbar->add("logout", new Text("<a href='logout.php'>Log Out</a>"));
-			/*$this->toolbar=[
-					'index' => new Text("<a href='index.php'>Home</a><br/>"), //all pages have back to index, except the index page, so it seems more efficient to just put the link in the default Page class
-					'user' => new Text("<a href='user.php'>User</a><br/>"),
-					'task' => new Text("<a href='task.php'>Task</a><br/>"),
-					//add more
-			];*/
-			$this->style= new Text(" <link rel='stylesheet' href='styles.css'>");
 		}
 		
 		function render() {
@@ -108,15 +122,9 @@
 			foreach ($this->head_items as $key => $value) {
 				$value->render();
 			}
+			echo "</head><body>",PHP_EOL;
 			//new toolbar render
 			$this->toolbar->render();
-			/*echo "</head><body>";
-			echo "<div style='color:#f44242; border-style:dotted;' align='left'>";
-			foreach ($this->toolbar as $key =>$value){
-				$value->render();
-			}
-			echo"</div>";
-			$this->toolbar[]= new Text("<a href='logout.php'>User</a><br/>");*///adds logout link to end of toolbar each time it is rendered. Consider a different approach
 			foreach ($this->body_items as $key => $value) {
 				$value->render();
 			}
--- www/index.php
+++ www/index.php
@@ -4,6 +4,11 @@
 		function __construct(){
 			parent::__construct();
 			$this->toolbar->remove('index');
+			$this->toolbar->style("font-style: italic;"); //test for style function from Div
+			$this->body_items[]=new Login();
+			for($i=1; $i<100; $i++){
+				$this->body_items[]=new Text("sample<br>");
+			}
 		}
 	}
 	$page=new Index();
