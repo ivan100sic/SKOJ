009083f75ec74e4523687dac1b939a1f5b4bfb99
LatentVirtue <asdf@asdf.org>
2017-05-27 00:59:22 +0200

    Revert "Meh nesto sto je zaostalo"
    This reverts commit 1210af42fbd3362c4211ff10cfe4f19068515905.

--- www/action.php
+++ www/action.php
@@ -1,18 +1,23 @@
 <?php
 require_once 'sql.php';
-require_once 'user.php';
 session_start();
 $name = $_POST['username'];
 $pass = $_POST['password'];
-$check = User::check($name, $pass);
-if($check==-2){
-	echo'name';
-}elseif ($check==-1){
-	echo 'pass';
-}elseif ($check>=1){
-	echo 'true';
-	$_SESSION['user']=$name;
-	$_SESSION['level']=SQL::get("SELECT level FROM users WHERE id = ?",[$check]);
+//define if functionality when username or password is empty
+$temp1=SQL::get("SELECT level FROM users WHERE username= ?", [$name]);
+$temp2=SQL::get("SELECT level FROM users WHERE password= ?", [$pass]);
+
+$tryname=$temp1[0]['level'];
+$trypass=$temp2[0]['level'];
+if($tryname != 0 && $trypass != 0 && $tryname==$trypass){
+	$_SESSION['username']=$name;
+	$_SESSION['level']=$tryname;
+	$_SESSION['status']=1;
+	header('Location: index.php');
+	exit();
+}else {
+	header('Location: index.php');
+	exit();
 }
 
 ?>
--- www/index.php
+++ www/index.php
@@ -1,6 +1,5 @@
 <?php
 	require_once 'dom.php';
-
 	class Index extends Page{
 		function __construct(){
 			parent::__construct();
@@ -9,40 +8,12 @@
 			if($_SESSION['status']==1){
 				$this->body_items[]=new EscapedText($_SESSION['username']);
 			}
-			$this->body_items[]=new LoginBox($_SESSION['status']);
+			$this->body_items[]=new Login($_SESSION['status']);
 			for($i=1; $i<100; $i++){
-				$this->body_items[]=new Text("sample \n");
+				$this->body_items[]=new Text("sample<br>");
 			}
 		}
 	}
 	$page=new Index();
-	$page->add_script(new Text("<script>
-$(document).ready(function(){
-	 $('#login').click(function(){	
-		  username=$('#name').val();
-		  password=$('#pass').val();
-		  $.ajax({
-		   type: 'POST',
-		   url: 'action.php',
-			data: 'name='+username+'&pwd='+password,
-		   success: function(html){    
-			if(html=='true')    {
-			 
-			}
-			else    {
-			$('#add_err').css('display', 'inline', 'important');
-			 $('#add_err').html('<img src='images/alert.png' />Wrong username or password');
-			}
-		   },
-		   beforeSend:function()
-		   {
-			$('#add_err').css('display', 'inline', 'important');
-			$('#add_err').html('<img src='images/ajax-loader.gif' /> Loading...')
-		   }
-		  });
-		return false;
-	});
-});
-</script>"));
 	$page->render();
 ?>
