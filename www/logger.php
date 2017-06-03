bf61ada1be78ebc5812b449ac675889cac804a45
Ivan Stosic <ivan100sic@gmail.com>
2017-06-03 17:09:53 +0200

    Commited missing logger.php'

---
+++ www/logger.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'global.php';
+
+class Logger {
+
+	static function log_raw($s) {
+		error_log($s, 3, '../logs/default.txt');
+	}
+
+	static function log($s, $level = 'Error') {
+		$dt = date("Y-m-d H:i:s");
+		$sid = get_session_id();
+		Logger::log_raw("[$level] [$dt] [session: $sid]: $s\n");
+	}
+
+	static function error($s) {
+		Logger::log($s, 'Error');
+	}
+
+	static function critical($s) {
+		Logger::log($s, 'CRITICAL');
+	}
+
+	static function notice($s) {
+		Logger::log($s, 'Notice');
+	}
+
+	static function warning($s) {
+		Logger::log($s, 'Warning');
+	}
+}
+
+
+
+?>
